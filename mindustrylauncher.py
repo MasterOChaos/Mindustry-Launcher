fb995dffedb340727063e1301c755fb4c278fed1
PhoenixOMystery <ThePhoenixOMystery@protonmail.com>
2020-12-18 12:06:21 -0500

    Use wxpython
    Instead of the non-crossplatform library gtk instead use wxpython for
    crossplatform usage

--- mindustrylauncher.py
+++ mindustrylauncher.py
@@ -1,120 +1,169 @@
-#!/usr/bin/env python
-# -*- Mode: Python; coding: utf-8; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
-import gi
-gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GdkPixbuf, Gdk
-import os, sys
-import os.path
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import wx, os, sys
 import urllib.request
+import os.path
 
-#UI file
-UI_FILE = "mindustrylauncher.ui"
+class MAINAPP(wx.Frame):
+	def __init__(self, *args, **kwds):
+		kwds["style"] = kwds.get("style", 0) | wx.DEFAULT_FRAME_STYLE
+		wx.Frame.__init__(self, *args, **kwds)
+		self.SetSize((220, 220))
+		self.SetTitle("Mindustry Launcher")
 
+		grid_sizer_1 = wx.GridSizer(3, 3, 0, 0)
 
-class GUI:
-	def __init__(self):
+		label_1 = wx.StaticText(self, wx.ID_ANY, "LATEST")
+		grid_sizer_1.Add(label_1, 0, wx.ALIGN_CENTER, 0)
 
-		self.builder = Gtk.Builder()
-		self.builder.add_from_file(UI_FILE)
-		self.builder.connect_signals(self)
+		self.pvl = wx.Button(self, wx.ID_ANY, "PLAY")
+		grid_sizer_1.Add(self.pvl, 0, wx.EXPAND, 0)
 
-		window = self.builder.get_object('window')
-		playlat = self.builder.get_object('playlatest')
-		play5 = self.builder.get_object('playv5')
-		play4 = self.builder.get_object('playv4')
-		uplat = self.builder.get_object('updatelatest')
-		up5 = self.builder.get_object('updatev5')
-		up4 = self.builder.get_object('updatev4')
-		
-		window.show_all()
+		self.uplat = wx.Button(self, wx.ID_ANY, "UPDATE")
+		grid_sizer_1.Add(self.uplat, 0, wx.EXPAND, 0)
 
-	def on_window_destroy(self, window):
-		Gtk.main_quit()
-		sys.exit("Exited Sucessfully")
-	
-	def on_playlatest_clicked(self, playlat):
+		label_2 = wx.StaticText(self, wx.ID_ANY, "V5")
+		grid_sizer_1.Add(label_2, 0, wx.ALIGN_CENTER, 0)
+
+		self.pv5 = wx.Button(self, wx.ID_ANY, "PLAY")
+		grid_sizer_1.Add(self.pv5, 0, wx.EXPAND, 0)
+
+		self.upv5 = wx.Button(self, wx.ID_ANY, "UPDATE")
+		grid_sizer_1.Add(self.upv5, 0, wx.EXPAND, 0)
+
+		label_3 = wx.StaticText(self, wx.ID_ANY, "V4")
+		grid_sizer_1.Add(label_3, 0, wx.ALIGN_CENTER, 0)
+
+		self.pv4 = wx.Button(self, wx.ID_ANY, "PLAY")
+		grid_sizer_1.Add(self.pv4, 0, wx.EXPAND, 0)
+
+		self.upv4 = wx.Button(self, wx.ID_ANY, "UPDATE")
+		grid_sizer_1.Add(self.upv4, 0, wx.EXPAND, 0)
+
+		self.SetSizer(grid_sizer_1)
+
+		self.Layout()
+
+		self.Bind(wx.EVT_BUTTON, self.pvl_click, self.pvl)
+		self.Bind(wx.EVT_BUTTON, self.uplat_click, self.uplat)
+		self.Bind(wx.EVT_BUTTON, self.pv5_click, self.pv5)
+		self.Bind(wx.EVT_BUTTON, self.upv5_click, self.upv5)
+		self.Bind(wx.EVT_BUTTON, self.pv4_click, self.pv4)
+		self.Bind(wx.EVT_BUTTON, self.upv4_click, self.upv4)
+		self.Bind(wx.EVT_CLOSE, self.CLOSED, self)
+
+	def pvl_click(self, event):
 		userpath = os.path.expanduser('~')
-		v6pathj = userpath + "/.cache/mindustrylauncher/v6/m.jar"
+		v6pathj = userpath + '/.cache/mindustrylauncher/v6/m.jar'
+		print("Launching Latest Version")
 		os.system("java -jar " + v6pathj)
-	def on_playv5_clicked(self, play5):
+		event.Skip()
+
+	def uplat_click(self, event):
+		userpath = os.path.expanduser('~')
+		v6pathj = userpath + '/.cache/mindustrylauncher/v6/m.jar'
+		v6path = userpath + "/.cache/mindustrylauncher/v6"
+		url3 = "https://github.com/Anuken/Mindustry/releases/download/v121.4/Mindustry.jar"
+		url2 = "https://github.com/Anuken/Mindustry/releases/download/v122/Mindustry.jar"
+		url1 = "https://github.com/Anuken/Mindustry/releases/download/v123/Mindustry.jar"
+		try:
+			urllib.request.urlretrieve(url3, v6pathj)
+			print('Downloaded V6')
+		except OSError as error:
+			os.remove(v6pathj)
+			urllib.request.urlretrieve(url3, v6pathj)
+			print('Downloaded V6')
+		except OSError as error:
+			os.makedirs(v6path)
+			urllib.request.urlretrieve(url3, v6pathj)
+			print('Downloaded V6')
+		except:
+			print("Failed to download")
+		event.Skip()
+
+	def pv5_click(self, event):
 		userpath = os.path.expanduser('~')
 		v5pathj = userpath + "/.cache/mindustrylauncher/v5/m.jar"
+		print("Launching V5")
 		os.system("java -jar " + v5pathj)
-	def on_playv4_clicked(self, play4m):
-		userpath = os.path.expanduser('~')
-		v4pathj = userpath + "/.cache/mindustrylauncher/v4/m.jar"
-		os.system("java -jar " + v4pathj)
-	def on_updatelatest_clicked(self, uplat):
-		userpath = os.path.expanduser('~')
-		v6path = userpath + "/.cache/mindustrylauncher/v6"
-		v6pathj = userpath + "/.cache/mindustrylauncher/v6/m.jar"
-		urll = 'https://github.com/Anuken/Mindustry/releases/download/v117.1/Mindustry.jar'
-		try:
-			os.mkdir(v6path)
-			urllib.request.urlretrieve(urll, v6pathj)
-			print('Downloaded Latest')
-			downwin = self.builder.get_object('downwindow')
-			downwin.show_all()
-		except OSError as error:
-			os.remove()
-			urllib.request.urlretrieve(urll, v6pathj)
-			print('Downloaded Latest')
-			downwin = self.builder.get_object('downwindow')
-			downwin.show_all()
-		except OSError as error:
-			urllib.request.urlretrieve(urll, v6pathj)
-			print('Downloaded Latest')
-			downwin = self.builder.get_object('downwindow')
-			downwin.show_all()
-	def on_updatev5_clicked(self, up5m):
+		event.Skip()
+
+	def upv5_click(self, event):
+		urlv5 = 'https://github.com/Anuken/Mindustry/releases/download/v104.6/Mindustry.jar'
 		userpath = os.path.expanduser('~')
 		v5path = userpath + "/.cache/mindustrylauncher/v5"
 		v5pathj = userpath + "/.cache/mindustrylauncher/v5/m.jar"
-		url5 = 'https://github.com/Anuken/Mindustry/releases/download/v104.6/Mindustry.jar'
 		try:
-			os.makedirs('cache/Mindustrylauncher/v5')
-			urllib.request.urlretrieve(url5, v5pathj)
+			urllib.request.urlretrieve(urlv5, v5pathj)
 			print('Downloaded V5')
-			downwin = self.builder.get_object('downwindow')
-			downwin.show_all()
 		except OSError as error:
-			os.remove('cache/Mindustrylauncher/v5/m.jar')
-			urllib.request.urlretrieve(url5, v5pathj)
+			os.remove(v5pathj)
+			urllib.request.urlretrieve(urlv5, v5pathj)
 			print('Downloaded V5')
-			downwin = self.builder.get_object('downwindow')
-			downwin.show_all()
 		except OSError as error:
-			urllib.request.urlretrieve(url5, v5pathj)
+			os.makedirs(v5path)
+			urllib.request.urlretrieve(urlv5, v5pathj)
 			print('Downloaded V5')
-			downwin = self.builder.get_object('downwindow')
-			downwin.show_all()
-	def on_updatev4_clicked(self, up4):
-		url4 = 'https://github.com/Anuken/Mindustry/releases/download/v96/Mindustry.jar'
+		except:
+			print("Failed to download")
+		event.Skip()
+
+	def pv4_click(self, event):
+		userpath = os.path.expanduser('~')
+		v4pathj = userpath + "/.cache/mindustrylauncher/v4/m.jar"
+		print("Launching V4")
+		os.system("java -jar " + v4pathj)
+		event.Skip()
+
+	def upv4_click(self, event):
+		urlv4 = 'https://github.com/Anuken/Mindustry/releases/download/v96/Mindustry.jar'
 		userpath = os.path.expanduser('~')
 		v4path = userpath + "/.cache/mindustrylauncher/v4"
 		v4pathj = userpath + "/.cache/mindustrylauncher/v4/m.jar"
 		try:
-			os.makedirs(v4path)
-			urllib.request.urlretrieve(url4, v4pathj)
+			urllib.request.urlretrieve(urlv4, v4pathj)
 			print('Downloaded V4')
-			downwin = self.builder.get_object('downwindow')
-			downwin.show_all()
 		except OSError as error:
 			os.remove(v4pathj)
-			urllib.request.urlretrieve(url4, v4pathj)
+			urllib.request.urlretrieve(urlv4, v4pathj)
 			print('Downloaded V4')
-			downwin = self.builder.get_object('downwindow')
-			downwin.show_all()
 		except OSError as error:
-			urllib.request.urlretrieve(url4, v4pathj)
+			os.makedirs(v4path)
+			urllib.request.urlretrieve(urlv4, v4pathj)
 			print('Downloaded V4')
-			downwin = self.builder.get_object('downwindow')
-			downwin.show_all()
-			
-def main():
-	app = GUI()
-	Gtk.main()
-		
+		except:
+			print("Failed to download")
+		event.Skip()
+
+	def CLOSED(self, event):
+		sys.exit("Exited Sucessfully")
+		event.Skip()
+
+# end of class MAINAPP
+
+class downframe(wx.Frame):
+	def __init__(self, *args, **kwds):
+		kwds["style"] = kwds.get("style", 0) | wx.DEFAULT_FRAME_STYLE
+		wx.Frame.__init__(self, *args, **kwds)
+		self.SetSize((164, 48))
+		self.SetTitle("Download Complete")
+
+		label_1 = wx.StaticText(self, wx.ID_ANY, "DOWNLOAD COMPLETE")
+		self.Layout()
+		self.Centre()
+
+# end of class downframe
+
+class MINDUSTRYLAUNCHER(wx.App):
+	def OnInit(self):
+		self.window = MAINAPP(None, wx.ID_ANY, "")
+		self.SetTopWindow(self.window)
+		self.window.Show()
+		return True
+
+# end of class MINDUSTRYLAUNCHER
 
 if __name__ == "__main__":
-	sys.exit(main())
+	APP = MINDUSTRYLAUNCHER(0)
+	APP.MainLoop()
